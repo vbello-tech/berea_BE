d01e82f1842c7e9077b27f154f796eb36eb63df7
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2026-08-30 21:23:03 +0100

    feat: change db to postgresql

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # berea_BE
+
+
+python manage.py migrate && python manage.py load_bible_json && python manage.py load_strongs && python manage.py load_tagged_greek_nt && python manage.py load_tagged_hebrew_ot &&  python manage.py load_cross_references    
--- berea/settings.py
+++ berea/settings.py
@@ -107,20 +107,20 @@ WSGI_APPLICATION = 'berea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_PUBLIC_URL'),
-#         conn_max_age=600,
-#         conn_health_checks=True,
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_PUBLIC_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
 
 
 # Password validation
