9dbbd8562e8857c847e6c0533096f700b2c28301
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2026-08-31 01:14:28 +0100

    chore: added FE to cors

--- berea/settings.py
+++ berea/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ.get("DEBUG", "False").strip().lower() == "true"
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
