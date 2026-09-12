316f5580eab8314d4971501ba251217ee749fff6
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2026-09-12 18:00:10 +0100

    feat: added ylt version

--- commands.sh
+++ commands.sh
@@ -1,23 +1,23 @@
 python manage.py migrate &&
 python manage.py load_bible_json &&
-python manage.py load_bible_json --file /Users/vbellotech/Desktop/START\ UP/Berea/Berea_BE/berea/bible/fixtures/kjv/bsb_bible.json --translation BSB &&
+python manage.py load_bible_json --file /Users/vbellotech/Desktop/START\ UP/Berea/Berea_BE/berea/bible/fixtures/kjv/ylt_bible.json --translation YLT &&
 python manage.py load_strongs &&
 python manage.py load_tagged_greek_nt &&
 python manage.py load_tagged_hebrew_ot &&
-python manage.py load_tagged_greek_nt --translation BSB &&
-python manage.py load_tagged_hebrew_ot --translation BSB &&
-python manage.py add_psalm_superscriptions &&
-python manage.py add_psalm_superscriptions --translation BSB &&
-python manage.py load_cross_references &&
-python manage.py load_cross_references --translation BSB
+python manage.py add_psalm_superscriptions && python manage.py load_cross_references &&
+
+python manage.py load_tagged_greek_nt --translation YLT && python manage.py load_tagged_hebrew_ot --translation YLT
+
+python manage.py add_psalm_superscriptions --translation YLT && python manage.py load_cross_references --translation YLT
+
 
 
 
 
 # ignore this commands for now.
-python manage.py load_kjv_strongs_tags
-python manage.py load_kjv_strongs_tags_from_sword --file /Users/vbellotech/Desktop/START\ UP/Berea/Berea_BE/berea/bible/fixtures/kjv_interlinear/kjv_strongs.json --clear
-python manage.py load_kjv_strongs_richtags --ot-file /Users/vbellotech/Desktop/START\ UP/Berea/Berea_BE/berea/bible/fixtures/kjv_hnt_got/kjv_TAHOT_merged.txt --nt-file /Users/vbellotech/Desktop/START\ UP/Berea/Berea_BE/berea/bible/fixtures/kjv_hnt_got/kjv_TAGNT_merged.txt   --clear
-
+#python manage.py load_kjv_strongs_tags
+#python manage.py load_kjv_strongs_tags_from_sword --file /Users/vbellotech/Desktop/START\ UP/Berea/Berea_BE/berea/bible/fixtures/kjv_interlinear/kjv_strongs.json --clear
+#python manage.py load_kjv_strongs_richtags --ot-file /Users/vbellotech/Desktop/START\ UP/Berea/Berea_BE/berea/bible/fixtures/kjv_hnt_got/kjv_TAHOT_merged.txt --nt-file /Users/vbellotech/Desktop/START\ UP/Berea/Berea_BE/berea/bible/fixtures/kjv_hnt_got/kjv_TAGNT_merged.txt   --clear
+#
 
 
